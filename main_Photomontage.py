5f084483f57ff6b2a5ef84808c55a17818c20a36
Sviatoslav Khmelevskyi <lechstanislaus@gmail.com>
2021-12-07 12:54:08 +0200

    Change the way of saving results

--- main_Photomontage.py
+++ main_Photomontage.py
@@ -1,9 +1,10 @@
 import numpy as np
 import imageio as im
+import matplotlib.pyplot as plt
 import func_Photomontage as ph
 
 I = np.array([np.array(im.imread("data\image_0"+str(i)+".png")).astype(int) for i in range(1,6)]).astype(int)
 M = (np.array([np.array(im.imread("data\mask_0"+str(i)+".png")).astype(int) for i in range(1,6)]) / 255).astype(int)
 
-Q = ph.Photomontage(I, M)
+plt.imsave('result.png', ph.Photomontage(I, M))
 print('Done')
